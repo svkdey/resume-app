fbfd5f3f1e2c8a5cc410e4626bf8152053f8bd0f
svkdey <svkdey9@gmail.com>
2020-02-20 22:36:54 +0530

    Updates

---
+++ precache-manifest.ebaf33ad63b9b673a10cba9c49e20676.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fefc9bff9376f20eef562d735141d19a",
+    "url": "/resume-app/index.html"
+  },
+  {
+    "revision": "051729e885c90eef67a9",
+    "url": "/resume-app/static/css/main.7e9c7b63.chunk.css"
+  },
+  {
+    "revision": "6d19cef5a75bd68dbbee",
+    "url": "/resume-app/static/js/2.b58d2046.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/resume-app/static/js/2.b58d2046.chunk.js.LICENSE"
+  },
+  {
+    "revision": "051729e885c90eef67a9",
+    "url": "/resume-app/static/js/main.d55b881e.chunk.js"
+  },
+  {
+    "revision": "3143e3d7f2a17ed09372",
+    "url": "/resume-app/static/js/runtime-main.679982b7.js"
+  },
+  {
+    "revision": "13b682b20bc4d98e6049e095b24d4689",
+    "url": "/resume-app/static/media/new.13b682b2.jpg"
+  }
+]);
